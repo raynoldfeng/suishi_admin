8606065813a4696e9f5109fcc05cbba2aaf881d5
kimiku <358218665@qq.com>
2019-09-10 23:21:56 +0800

    q

--- curriculumSystem/src/components/com/pairTypeMenus.vue
+++ curriculumSystem/src/components/com/pairTypeMenus.vue
@@ -9,14 +9,14 @@
             <el-input class="input_type" v-model="nowData.pairTypeInfo"></el-input>
         </div>
         <div class="view_main">
-            <el-button @click="">添加问题</el-button>
+            <el-button @click="addQEvent">添加问题</el-button>
         </div>
         <div class="view_main">
             <ul class="padd_css">
                 <li v-for="(data,index) in nowData.pairTypeMenu">
                     <div class="view_main">
-                        <el-button @click="">删除问题</el-button>
-                    <el-button @click="">添加答案</el-button>
+                        <el-button @click="deleteQEvent(index)">删除问题</el-button>
+                        <el-button @click="addAEvent(index)">添加答案</el-button>
                     </div>
                     <div class="view_main">
                         <span>问题标题:</span>
@@ -25,7 +25,7 @@
                     <ul class="padd_css">
                         <li v-for="(sdata,sindex) in data.pairTypeAmenu">
                             <div class="view_main">
-                                <el-button @click="">删除答案</el-button>
+                                <el-button @click="deleteAEvent(index,sindex)">删除答案</el-button>
                             </div>
                             <div class="view_main">
                                 <span>答案标题:</span>
@@ -69,7 +69,44 @@ export default {
 
     },
     methods:{
+        addQEvent(){
 
+            var index = this.nowData.pairTypeMenu.length;
+            var newQdata = {
+                pairTypeQtitle:"",
+                pairTypeQindex:index,
+                pairTypeAmenu:[
+                    {
+                        pairTypeAtitle:"",
+                        pairTypeAcontext:"",
+                        pairTypeAindex:index
+                    }
+                ]
+            };
+            this.nowData.pairTypeMenu.push(newQdata);
+        },
+        deleteQEvent(index){
+            this.nowData.pairTypeMenu.splice(index,1);
+
+            for(let i = 0;i < this.nowData.pairTypeMenu.length;i++){
+                this.nowData.pairTypeMenu[i].pairTypeQindex = i;
+                for(let l=0;l<this.nowData.pairTypeMenu[i].pairTypeAmenu.length;l++){
+                    this.nowData.pairTypeMenu[i].pairTypeAmenu[l].pairTypeAindex = i;
+                }
+            }
+        },
+        addAEvent(index){
+            var newAdata =  {
+                pairTypeAtitle:"",
+                pairTypeAcontext:"",
+                pairTypeAindex:index
+            };
+            this.nowData.pairTypeMenu[index].pairTypeAmenu.push(newAdata);
+        },
+        deleteAEvent(index,sindex){
+            console.log(index);
+            this.nowData.pairTypeMenu[index].pairTypeAmenu.splice(sindex,1);
+        }
 
     }
 }

0cadaf0d13c625c530bad06d8bb464f54e92156d
kimiku <358218665@qq.com>
2019-09-13 22:16:00 +0800

    dd

--- curriculum/src/components/dragmore.vue
+++ curriculum/src/components/dragmore.vue
@@ -6,13 +6,17 @@
             <ul class="question-menu">
                 <li v-for="(sdata,index) in data.pairTypeMenu">
                     <p class="drag-text" v-text="sdata.pairTypeQtitle"></p>
-                    <div class="drag-box" :class="{clicked:ispair}"  @click="pairEvent(sdata.pairTypeQindex)"></div>
+                    <div class="drag-menu">
+                        <div class="finish-css" v-for="(adata,aindex) in readMenu" v-if="sdata.pairTypeQindex == adata.pairTypeAindex" v-text="adata.pairTypeAcontext"></div>
+                        <div class="drag-box" :class="{clicked:ispair}"  @click="pairEvent(sdata.pairTypeQindex)"></div>
+                    </div>
+
                 </li>
             </ul>
         </div>
         <!--☰-->
         <div class="answer-menu">
-            <div  class="animated dragboxbtn " :class="{clicked:ispair,shake:istrue == 'false'}" @click="pairChange" v-html="nowAnswerText"></div>
+            <div  class="animated dragboxbtn " v-show="isfinish == false"  :class="{clicked:ispair,shake:istrue == 'false'}" @click="pairChange" v-html="nowAnswer.pairTypeAtitle"></div>
         </div>
 
 </div>
@@ -26,11 +30,15 @@ export default
     },
     data(){
         return{
-            newAMenu:[],
+            allAMenu:[],
+            unAMenu:[],
+            readMenu:[],
             nowAnswer:"",
-            ispair:false,
-            istrue:false,
-            nowAnswerText:""
+            ispair:false, //是否点击
+            istrue:false, //是否正确
+            dataIndex:0,
+            allNum:0,
+            isfinish:false
         }
     },
     methods:{
@@ -38,15 +46,32 @@ export default
             return str.replace(/\n|\r\n/g,"<br/>");
         },
         aShowEvent(){
-            console.log(this.newAMenu);
-            console.log(this.newAMenu[0].pairTypeAindex)
-            this.nowAnswer = this.newAMenu[0].pairTypeAindex;
-            this.nowAnswerText = this.newAMenu[0].pairTypeAtitle;
+            if(this.allAMenu.length == 1){
+                this.dataIndex = 0;
+            }else{
+                this.dataIndex =  parseInt(Math.random()*this.allAMenu.length);
+            }
+            this.nowAnswer = this.allAMenu[this.dataIndex];
+            this.allAMenu.splice(this.dataIndex,1);
         },
         pairEvent(index){
-            if(this.nowAnswer == index){
-                alert(1);
-                this.istrue = "false";
+            var _this = this;
+            if(this.ispair == true){
+                if(this.nowAnswer.pairTypeAindex == index){
+                    this.readMenu.push(this.nowAnswer);
+                    this.pairChange();
+                    if(this.readMenu.length < this.allNum){
+                        this.aShowEvent();
+                    }else{
+                        this.isfinish = true;
+                    }
+                }else{
+                    this.istrue = "false";
+                    this.ispair = !this.ispair;
+                    setTimeout(function(){
+                        _this.istrue = "";
+                    },600);
+                }
             }
         },
         pairChange(){
@@ -55,15 +80,15 @@ export default
         }
     },
     mounted:function(){
-        this.newAMenu = [];
+        this.allAMenu = [];
         for(let i = 0; i < this.data.pairTypeMenu.length;i++){
-            this.newAMenu =  $.merge(this.newAMenu,this.data.pairTypeMenu[i].pairTypeAmenu);
+            this.allAMenu =  $.merge(this.allAMenu,this.data.pairTypeMenu[i].pairTypeAmenu);
         }
+        this.allNum = this.allAMenu.length;
+        this.aShowEvent();
     },
     watch:{
-        newAMenu(){
-            this.aShowEvent();
-        }
+
     }
 }
 </script>
@@ -83,16 +108,29 @@ export default
         font-size: 4.5vw;
         word-break:break-all;
     }
-    .drag-box{
+    .drag-menu{
         width: 48%;
-        height: 40px;
+        float: right;
         margin: 0 0 0 2%;
-        float: left;
+        font-size: 4.5vw;
+    }
+    .drag-box{
+        width: 98%;
+        height: 40px;
         border: 1px solid #ddd;
     }
     .drag-box.clicked{
         background: #efefef;
         color: #ffffff;
+    }
+    .finish-css{
+        width: 92%;
+        margin: 0 0 2% 2%;
+        background: #286b36;
+        padding: 1% 4%;
+        color: #ffffff;
+        text-align: left;
+        font-size: 4vw;;
     }
     .dragboxbtn {
         max-width: 80%;
@@ -128,4 +166,5 @@ export default
     background: #999999;
     color: #ffffff;
 }
+
 </style>
